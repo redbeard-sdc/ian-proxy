b09cb8d1a2f1a61a2f8dd323f3f64dedb345074c
Jared Brink <J-radical@Jareds-MacBook-Pro.local>
2019-04-16 13:46:46 -0600

    Added styling to template.

--- server.js
+++ server.js
@@ -10,9 +10,33 @@ const proxy = httpProxy.createProxyServer({});
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.all("/header/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-3-17-179-159.us-east-2.compute.amazonaws.com/"
+  });
+});
+
 app.all("/hotels/*", (req, res) => {
   proxy.web(req, res, {
     target: "http://ec2-18-188-161-215.us-east-2.compute.amazonaws.com/"
+  });
+});
+
+// app.all("/prices/*", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://ec2-18-222-174-176.us-east-2.compute.amazonaws.com/"
+//   });
+// });
+
+app.all("/location/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-54-215-233-5.us-west-1.compute.amazonaws.com/"
+  });
+});
+
+app.all("/api/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-18-222-0-232.us-east-2.compute.amazonaws.com/"
   });
 });
 
